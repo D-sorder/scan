0711fb68f166929501c25e794a74ce93fff5c06b
disorder <creepsta6001@gmail.com>
2024-12-25 20:19:30 +0700

    da

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/header";
+import Main from "./components/Main/main";
+import Footer from "./components/Footer/footer";
+import Error from "./components/Error-page/error";
+import Authorization from "./components/Authorization/auth";
+import Search from "./components/Search/search";
+import SearchResult from "./components/SearchResult/searchResult";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const routesData = [
+  { path: "*", element: <Main /> },
+  { path: "/error", element: <Error /> },
+  { path: "/auth", element: <Authorization /> },
+  { path: "/search", element: <Search /> },
+  { path: "/result", element: <SearchResult /> },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            {routesData.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
